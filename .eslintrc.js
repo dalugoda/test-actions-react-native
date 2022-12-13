e4997dfbf978ddd551244b685263fd900f4faeb6
Sandupa Santhusha <sandupasanthusha@gmail.com>
2022-12-13 15:48:17 +0800

    Add test changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,25 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:react/recommended',
-    'standard'
+    "plugin:react/recommended",
+    "standard"
   ],
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 12,
+    sourceType: "module"
   },
   plugins: [
-    'react'
+    "react"
   ],
   rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
-}
+};
