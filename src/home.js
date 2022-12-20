9910bcbefb3d4b5c099c2aea658e4fdec231e240
Sandupa Dalugoda <31846829+dalugoda@users.noreply.github.com>
2022-12-20 12:10:13 +0800

    Update home.js

--- src/home.js
+++ src/home.js
@@ -2,5 +2,19 @@ import React from "react";
 import { View } from "react-native";
 
 export const TestApp = () => {
+  
+  function bar() {
+      var p = 'clean';
+      function() {
+          var obj = { dirty: 'dirty' };
+          for (p in obj) { // Oh no, missing 'var' here!
+              obj[p] = obj[p];
+          }
+          return x;
+      }();
+
+      // 'p' is trashed and has value of 'dirty'!
+  }
+  
   return <View />;
 };
